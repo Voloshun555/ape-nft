87dc98f85f10278b8a5c0d4fbe220bf11c59466d
Voloshun555 <Voloshunihor@gmail.com>
2024-03-24 15:22:29 +0200

    add section FAQ

---
+++ src/components/data/collectionFaq.js
@@ -0,0 +1,39 @@
+import faq1x1 from "../../assets/images/faq/1faq1x.png"
+import faq1x2 from "../../assets/images/faq/1faq2x.png"
+import faq2x1 from "../../assets/images/faq/2faq1x.png"
+import faq2x2 from "../../assets/images/faq/2faq2x.png"
+import faq3x1 from "../../assets/images/faq/3faq1x.png"
+import faq3x2 from "../../assets/images/faq/3faq2x.png"
+import faq4x1 from "../../assets/images/faq/4faq1x.png"
+import faq4x2 from '../../assets/images/faq/4faq2x.png';
+
+export const faqCollections = [
+  {
+    image1x: faq1x1,
+    image2x: faq1x2,
+    alt: 'Ape',
+    title: 'WHAT IS AN NFT COLLECTION?',
+    text: 'An NFT collection is a group of unique digital assets, each represented by a non-fungible token, typically created around a specific theme or style.',
+  },
+  {
+    image1x: faq2x1,
+    image2x: faq2x2,
+    alt: 'Ape',
+    title: 'HOW DO I PURCHASE NFTS FROM A COLLECTION?',
+    text: 'To purchase nfts from a collection, you typically need to use cryptocurrency on a blockchain0based marketplace.',
+  },
+  {
+    image1x: faq3x1,
+    image2x: faq3x2,
+    alt: 'Ape',
+    title: 'CAN I SELL OR TRADE NFTS FROM A COLLECTION?',
+    text: 'Yes, you can sell or trade NFTs from a collection like you would other digital assets.',
+  },
+  {
+    image1x: faq4x1,
+    image2x: faq4x2,
+    alt: 'Ape',
+    title: 'WHAT RIGHTS DO I HAVE AS AN OWNER OF AN NFT?',
+    text: "As an NFT owner, you can own, transfer, potentially access exclusive content, resell, but don't automatically get copyright or intellectual property rights.",
+  },
+];
--- src/components/faq/faq.jsx
+++ src/components/faq/faq.jsx
@@ -1,17 +1,35 @@
-import s from './faq.module.scss'
+import React, { useState } from 'react';
+import { faqCollections } from '../data/collectionFaq';
+import s from './faq.module.scss';
 
 export const Faq = () => {
+  const [activeItem, setActiveItem] = useState('1');
+
   return (
     <section className={s.section}>
       <div className={s.container}>
         <h2 className={s.title}>FAQ</h2>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+        <ul className={s.wrapCollections}>
+          {faqCollections.map((item, index) => (
+            <li
+              key={index}
+              onClick={() => setActiveItem(String(index + 1))}
+              className={`${s.listColection} ${activeItem === String(index + 1) ? s.active : ''}`}
+            >
+              <div className={s.wrapText}>
+                <h3 className={s.titleColections}>{item.title}</h3>
+                <p className={s.additionalInfo}>{item.text}</p>
+                <img
+                  srcSet={`${item.image1x} 1x, ${item.image2x} 2x`}
+                  src={item.image1x}
+                  alt={item.alt}
+                  className={s.imgFAQ}
+                />
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </section>
   );
-}
+};
